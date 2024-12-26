75f7b71f8e8f581deaa3b0b54dd0fcfa9bacd6f3
Felipe Cristovão <felipecss88@gmail.com>
2024-12-25 21:10:33 -0300

    aplicação mobile configurada

---
+++ mobile/src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Bem-vindo ao App de Entregas!</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
