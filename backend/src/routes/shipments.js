d2ec28378db0985ce83623cdd0f99002e1c38cbe
Felipe Cristovão <felipecss88@gmail.com>
2025-01-01 18:38:30 -0300

    chave secreta ajustada

--- backend/src/routes/shipments.js
+++ backend/src/routes/shipments.js
@@ -14,7 +14,7 @@ const authenticateDriver = (req, res, next) => {
     return res.status(401).json({ error: 'Token não fornecido' });
   }
 
-  jwt.verify(token, 'SECRET_KEY', (err, decoded) => {
+  jwt.verify(token,process.env.SECRET_KEY, (err, decoded) => {
     if (err) {
       return res.status(403).json({ error: 'Token inválido' });
     }
